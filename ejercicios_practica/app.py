c36296e240dbb0c40da20df295d11340fa8fd1c0
lucianosemba <93680096+lucianosemba@users.noreply.github.com>
2022-03-28 16:46:15 -0300

    Add files via upload

--- ejercicios_practica/app.py
+++ ejercicios_practica/app.py
@@ -60,8 +60,17 @@ def personas():
         # Debe verificar si el limit y offset son válidos cuando
         # no son especificados en la URL
 
+        limit_str = str(request.args.get('limit'))
+        offset_str = str(request.args.get('offset'))
+
         limit = 0
         offset = 0
+
+        if(limit_str is not None) and (limit_str.isdigit()):
+            limit = int(limit_str)
+
+        if(offset_str is not None) and (offset_str.isdigit()):
+            offset = int(offset_str)
 
         result = persona.report(limit=limit, offset=offset)
         return jsonify(result)
@@ -78,12 +87,12 @@ def registro():
             age = 0
             # Alumno:
             # Obtener del HTTP POST JSON el nombre y los pulsos
-            # name = ...
-            # age = ...
+            name = str(request.form.get('name'))
+            age = str(request.form.get('age'))
 
             # Alumno: descomentar la linea persona.insert una vez implementado
             # lo anterior:
-            # persona.insert(name, int(age))
+            persona.insert(name, int(age))
             return Response(status=200)
         except:
             return jsonify({'trace': traceback.format_exc()})
@@ -106,9 +115,9 @@ def comparativa():
 
         # Descomentar luego de haber implementado su función en persona.py:
 
-        # x, y = persona.dashboard()
-        # image_html = utils.graficar(x, y)
-        # return Response(image_html.getvalue(), mimetype='image/png')
+        x, y = persona.dashboard()
+        image_html = utils.graficar(x, y)
+        return Response(image_html.getvalue(), mimetype='image/png')
 
         return "Alumno --> Realice la implementacion"
     except:
